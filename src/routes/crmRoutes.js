8cc12e0475c4e12c5b689a51a08ee55587a4c4af
Anton Kuznetsov <antakuznet@gmail.com>
2022-07-22 19:36:46 -0700

    Added middleware to the get method for contact

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,9 +1,17 @@
 const routes = (app) => {
   app
     .route("/contact")
-    .get((req, res) => {
-      res.send("Get request successful");
-    })
+    .get(
+      (req, res, next) => {
+        // middleware
+        console.log(`Request came from: ${req.originalUrl}`);
+        console.log(`Request type: ${req.method}`);
+        next();
+      },
+      (req, res, next) => {
+        res.send("Get request successful");
+      }
+    )
     .post((req, res) => {
       res.send("Post request successful");
     });
